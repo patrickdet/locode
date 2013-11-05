6adbb438aa6675c4348a3d787f220ce4aba76d08
Niels Stevens <niels@kabisa.nl>
2013-11-05 16:17:48 +0100

    Replace " quotes with ' quotes where no interpolation is needed Reformat code

--- lib/locode.rb
+++ lib/locode.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 require 'yaml'
-require_relative "locode/version"
-require_relative "locode/location"
+require_relative 'locode/version'
+require_relative 'locode/location'
 
 module Locode
+
   def self.load_data
     YAML.load(File.read(File.expand_path('../../data/yaml/dump.yml', __FILE__)))
   end
@@ -12,38 +13,37 @@ module Locode
   ALL_LOCATIONS = load_data
   private_constant :ALL_LOCATIONS
 
-
   def self.seaports(limit = ALL_LOCATIONS.size)
-    ALL_LOCATIONS.select {|location| location.seaport? }.take(limit)
+    ALL_LOCATIONS.select { |location| location.seaport? }.take(limit)
   end
 
   def self.rail_terminals(limit = ALL_LOCATIONS.size)
-    ALL_LOCATIONS.select {|location| location.rail_terminal? }.take(limit)
+    ALL_LOCATIONS.select { |location| location.rail_terminal? }.take(limit)
   end
 
   def self.road_terminals(limit = ALL_LOCATIONS.size)
-    ALL_LOCATIONS.select {|location| location.road_terminal? }.take(limit)
+    ALL_LOCATIONS.select { |location| location.road_terminal? }.take(limit)
   end
 
-  def self.airport(limit = ALL_LOCATIONS.size)s
-    ALL_LOCATIONS.select {|location| location.airport? }.take(limit)
+  def self.airport(limit = ALL_LOCATIONS.size)
+    ALL_LOCATIONS.select { |location| location.airport? }.take(limit)
   end
 
   def self.postal_exchange_offices(limit = ALL_LOCATIONS.size)
-    ALL_LOCATIONS.select {|location| location.postal_exchange_office? }.take(limit)
+    ALL_LOCATIONS.select { |location| location.postal_exchange_office? }.take(limit)
   end
 
   def self.inland_clearance_depots(limit = ALL_LOCATIONS.size)
-    ALL_LOCATIONS.select {|location| location.inland_clearance_depot? }.take(limit)
+    ALL_LOCATIONS.select { |location| location.inland_clearance_depot? }.take(limit)
   end
 
   # the spec says these are currently just oil platforms
   def self.fixed_transport_functions(limit = ALL_LOCATIONS.size)
-    ALL_LOCATIONS.select {|location| location.fixed_transport_functions? }.take(limit)
+    ALL_LOCATIONS.select { |location| location.fixed_transport_functions? }.take(limit)
   end
 
   def self.border_crossing_functions(limit = ALL_LOCATIONS.size)
-    ALL_LOCATIONS.select {|location| location.border_crossing? }.take(limit)
+    ALL_LOCATIONS.select { |location| location.border_crossing? }.take(limit)
   end
 
   # Public: Find Locations that partially match the Search String.
@@ -67,9 +67,8 @@ module Locode
   # Returns an Array of Location
   def self.find_by_locode(search_string)
     return [] unless search_string && search_string.is_a?(String)
-    ALL_LOCATIONS.select {|location| location.locode.start_with?(search_string.strip.upcase) }
+    ALL_LOCATIONS.select { |location| location.locode.start_with?(search_string.strip.upcase) }
   end
-
 
   # Public: Find Locations whose full name or full name without diacritics
   #         matches the search string
@@ -93,7 +92,7 @@ module Locode
       names << location.full_name_without_diacritics if location.full_name_without_diacritics
       names += location.alternative_full_names
       names += location.alternative_full_names_without_diacritics
-      names.map {|name| name.downcase }.any? {|name| name.start_with?(search_string.strip.downcase) }
+      names.map { |name| name.downcase }.any? { |name| name.start_with?(search_string.strip.downcase) }
     end
   end
 end
